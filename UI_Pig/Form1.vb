495c6e8ffc74e332455f96db935c9ed953d7d0b1
Julian <juenchato@gmail.com>
2017-02-04 14:12:00 -0600

    Despues del primer turno de la PC solo le sale 1.

--- UI_Pig/Form1.vb
+++ UI_Pig/Form1.vb
@@ -6,6 +6,7 @@
 
 
     Public Sub turnoPC()
+        turnosPC = 0
         acumulado = 0
         Dim turnos As Integer
 
@@ -13,29 +14,41 @@
         Dim random As Integer = CInt(Int((3 * Rnd()) + 1))
         If random = 1 Then
             turnos = turnosJugador + 1
-        ElseIf random = 2 Then
-            turnos = turnosJugador
-        Else
+        ElseIf random = 2 And turnosJugador > 1 Then
             turnos = turnosJugador - 1
+        ElseIf random = 3 Then
+            turnos = turnosJugador
         End If
+
         While turnosPC <= turnos
+            Randomize()
             Dim numero As Integer = CInt(Int((6 * Rnd()) + 1))
             txtDado.Text = numero
 
             If numero = 1 Then
-                MsgBox("Ha salido un 1.  El turno pasa al siguente jugador.", MsgBoxStyle.Information, "Fin del turno")
+                'turnosPC = turnosJugador + 1
+                MsgBox("Ha salido un 1.  El turno pasa al Jugador.", MsgBoxStyle.Information, "Fin del turno")
+                acumulado = 0
+                txtAcumulado.Text = acumulado
                 txtDado.Text = 0
-                turnosPC = turnos + 1
+                Exit While
 
             ElseIf numero <> 1 Then
                 turnosPC = turnosPC + 1
                 acumulado = acumulado + numero
                 txtAcumulado.Text = acumulado
-                txtPC.Text = Val(txtPC.Text) + Val(txtAcumulado.Text)
 
             End If
 
         End While
+
+        If turnosPC = turnosJugador Then
+            MsgBox("Termino mi turno", MsgBoxStyle.Information, "PC")
+            txtPC.Text = Val(txtPC.Text) + Val(txtAcumulado.Text)
+            txtAcumulado.Text = "0"
+            txtDado.Text = "0"
+        End If
+
         If Val(txtPC.Text) >= 100 Then
             MsgBox("100pts! ¡Mas suerte la proxima!", MsgBoxStyle.Information, "Fin del juego")
             txtAcumulado.Text = "0"
@@ -43,10 +56,12 @@
             txtJugador.Text = "0"
             txtPC.Text = "0"
         End If
-        MsgBox("Termino mi turno", MsgBoxStyle.Information, "Fin del turno")
-        acumulado = 0
-        txtAcumulado.Text = acumulado
+        txtAcumulado.Text = "0"
+        txtDado.Text = "0"
         turnosPC = 0
+        acumulado = 0
+        'btnTirar.Visible = True
+        'btnFinTurno.Visible = True
     End Sub
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblJugador.Click
@@ -64,11 +79,14 @@
         txtDado.Text = numero
 
         If numero = 1 Then
-            MsgBox("Ha salido un 1.  El turno pasa al siguente jugador.", MsgBoxStyle.Information, "Fin del turno")
-            txtDado.Text = 0
-            turnoPC()
+            MsgBox("Ha salido un 1.  El turno pasa a la PC.", MsgBoxStyle.Information, "Fin del turno")
             acumulado = 0
             txtAcumulado.Text = acumulado
+            numero = 0
+            txtDado.Text = 0
+            turnoPC()
+            'btnTirar.Visible = False
+            'btnFinTurno.Visible = False
         ElseIf numero <> 1 Then
             acumulado = acumulado + numero
             txtAcumulado.Text = acumulado
@@ -85,16 +103,17 @@
 
     Private Sub btnFinTurno_Click(sender As Object, e As EventArgs) Handles btnFinTurno.Click
         txtJugador.Text = Val(txtJugador.Text) + Val(txtAcumulado.Text)
-        acumulado = 0
-        txtAcumulado.Text = acumulado
+
         If Val(txtJugador.Text) >= 100 Then
             MsgBox("100pts! ¡Felicidades!", MsgBoxStyle.Information, "Fin del juego")
             txtAcumulado.Text = "0"
             txtDado.Text = "0"
             txtJugador.Text = "0"
-
+        ElseIf Val(txtJugador.Text) < 100 Then
+            MsgBox("Termino mi turno", MsgBoxStyle.Information, "Jugador")
+            txtAcumulado.Text = "0"
+            txtDado.Text = "0"
+            turnoPC()
         End If
-        MsgBox("Termino mi turno", MsgBoxStyle.Information, "Fin del turno")
-        turnoPC()
     End Sub
 End Class
